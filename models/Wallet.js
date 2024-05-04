74f2faa5336aeab2a3c75a850ec135feae61b06d
tobby340602 <niklaussergei23@gmail.com>
2024-05-04 17:58:13 +1000

    removed mongo connection, changed parameters

--- models/Wallet.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  marketid: String,
-  scretkey: String,
-},{ timestamps: true});
-
-module.exports = mongoose.model('Wallet', userSchema);
